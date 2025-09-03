415ee1f7a973a23d5718ed581249bee760241d15
Ishagp <gisha825@gmail.com>
2025-09-03 23:49:42 +0530

    React router

---
+++ Learning_React-main/01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'   //hooks from react
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>This is my first project</h1>
+    </>
+  )
+}
+
+export default App
